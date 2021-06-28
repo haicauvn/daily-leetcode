2b5a151c91698916cf0b046a16b52086674c89a6
Hai Dang <haidv245@gmail.com>
2021-06-29 00:20:52 +0700

    2021.06.29 isSubsequence

---
+++ src/haicauvn/dailyleetcode/IsSubsequence.java
@@ -0,0 +1,30 @@
+package haicauvn.dailyleetcode;
+
+/**
+ * https://leetcode.com/problems/is-subsequence/
+ */
+
+public class IsSubsequence {
+	public static void main(String[] args) {
+		IsSubsequence solution = new IsSubsequence();
+		System.out.println(solution.isSubsequence("aaaaaa", "bbaaaa"));
+	}
+
+	public boolean isSubsequence(String s, String t) {
+		if (s.length() == 0) {
+			return true;
+		}
+		if (s.length() > t.length()) {
+			return false;
+		}
+		for (int i = 0; i < s.length(); i++) {
+			int index = t.indexOf(s.charAt(i));
+			if (index >= 0) {
+				t = t.substring(index + 1, t.length());
+			} else {
+				return false;
+			}
+		}
+		return true;
+	}
+}
