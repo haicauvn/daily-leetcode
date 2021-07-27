1983eabd0936df225cd6604ceb6e81eef67a935c
Hai Dang <haidv245@gmail.com>
2021-07-27 14:40:09 +0700

    2021.07.27 removeElement

---
+++ src/haicauvn/dailyleetcode/array/_27_Remove_Element.java
@@ -0,0 +1,21 @@
+package haicauvn.dailyleetcode.array;
+
+import java.util.Arrays;
+
+public class _27_Remove_Element {
+    public static void main(String[] args) {
+        int[] a = new int[]{0,1,2,2,3,0,4,2};
+        System.out.println(removeElement(a, 2));
+        System.out.println(Arrays.toString(a));
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if(nums[j] != val) {
+                nums[i++] = nums[j];
+            }
+        }
+        return i;
+    }
+}
